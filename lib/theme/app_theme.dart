120a22fb9a38a79a8dc67fbc8849bcaf51826efd
Fiesty_Cushion <neupane.braz07@gmail.com>
2023-04-26 23:57:38 +0545

    Changed some themes

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:attendy/theme/pallete.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class AppTheme {
   static ThemeData theme = ThemeData.dark().copyWith(
+    dividerColor: HexColor('#F0F0F0'),
+    cardColor: Colors.white,
+    textTheme: GoogleFonts.poppinsTextTheme(),
     scaffoldBackgroundColor: Pallete.backgroundColor,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Pallete.backgroundColor,
+    appBarTheme: AppBarTheme(
+      titleTextStyle: GoogleFonts.poppins(
+        color: Colors.white,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+      ),
+      backgroundColor: Colors.black,
       elevation: 0,
-    ),
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: Pallete.blueColor,
     ),
   );
 }
