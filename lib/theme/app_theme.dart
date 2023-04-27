dc267e1558cd73367251b997c614e9e5fb88175c
Fiesty_Cushion <neupane.braz07@gmail.com>
2023-04-27 17:37:25 +0545

    UI Fix

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class AppTheme {
-  static ThemeData theme = ThemeData.dark().copyWith(
+  static ThemeData theme = ThemeData.light().copyWith(
     dividerColor: HexColor('#F0F0F0'),
     cardColor: Colors.white,
     textTheme: GoogleFonts.poppinsTextTheme(),
