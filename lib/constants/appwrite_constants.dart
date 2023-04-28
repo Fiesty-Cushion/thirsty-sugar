39527a3b2c392a07e85fca3e9c8108794adbc8db
Fiesty_Cushion <neupane.braz07@gmail.com>
2023-04-28 16:40:36 +0545

    Constants Changed

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,5 +1,7 @@
 class AppwriteConstants {
   static const String databaseId = '6447916e7fd0bb75c420';
   static const String projectId = '6447902e019c8d10f26a';
-  static const String endPoint = 'http://192.168.101.4:80/v1';
+  static const String endPoint = 'http://192.168.101.14:80/v1';
+
+  static const String usersCollectionId = '644a6ba4e9e3e9b047e0';
 }
