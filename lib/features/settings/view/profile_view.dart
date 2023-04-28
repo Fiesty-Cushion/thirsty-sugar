aa6f05876c7233494e1d88cd8d9c5d7f741b3f5e
Fiesty_Cushion <neupane.braz07@gmail.com>
2023-04-28 16:42:47 +0545

    UI Changes

--- lib/features/settings/view/profile_view.dart
+++ lib/features/settings/view/profile_view.dart
@@ -23,119 +23,30 @@ class _ProfileViewState extends ConsumerState<ProfileView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: HexColor('#F0F0F0'),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: [
-            // User card
-            BigUserCard(
-              backgroundColor: HexColor('#F44133'),
-              userName: "Fiesty Cushion",
-              userProfilePic: const NetworkImage(
-                  'https://pbs.twimg.com/profile_images/1423537244220968963/HHT7Mg8l_400x400.jpg'),
-              cardActionWidget: ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: Material(
-                  color: Colors.white,
-                  child: InkWell(
-                    child: SettingsItem(
-                      icons: Icons.edit_outlined,
-                      iconStyle: IconStyle(
-                        backgroundColor: Colors.white,
-                        iconsColor: Colors.black,
-                      ),
-                      title: "Modify",
-                      subtitle: "Tap to change your data",
-                      onTap: () {},
-                      trailing: const Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.grey,
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            MySettingsGroup(
-              items: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15.0),
-                    topRight: Radius.circular(15.0),
-                  ),
-                  child: Material(
-                    color: Colors.white,
-                    child: InkWell(
-                      child: SettingsItem(
-                        onTap: () {},
-                        icons: Icons.brush_rounded,
-                        iconStyle: IconStyle(
-                          backgroundColor: Colors.white,
-                          iconsColor: Colors.black,
-                        ),
-                        title: 'Appearance',
-                        subtitle: "Make the App yours",
-                        trailing: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.grey,
-                          size: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(15.0),
-                    bottomRight: Radius.circular(15.0),
-                  ),
-                  child: Material(
-                    color: Colors.white,
-                    child: InkWell(
-                      child: SettingsItem(
-                        onTap: () {
-                          setState(() {
-                            isSwitchOn = !isSwitchOn;
-                          });
-                        },
-                        icons: Icons.dark_mode_outlined,
-                        iconStyle: IconStyle(
-                          backgroundColor: Colors.white,
-                          iconsColor: Colors.black,
-                        ),
-                        title: 'Dark mode',
-                        subtitle: "Automatic",
-                        trailing: Switch.adaptive(
-                          value: isSwitchOn,
-                          onChanged: (newValue) {
-                            setState(() {
-                              isSwitchOn = newValue;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            ClipRRect(
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: [
+          // User card
+          BigUserCard(
+            backgroundColor: HexColor('#F44133'),
+            userName: "Fiesty Cushion",
+            userProfilePic: const NetworkImage(
+                'https://pbs.twimg.com/profile_images/1423537244220968963/HHT7Mg8l_400x400.jpg'),
+            cardActionWidget: ClipRRect(
               borderRadius: BorderRadius.circular(15.0),
               child: Material(
                 color: Colors.white,
                 child: InkWell(
                   child: SettingsItem(
-                    onTap: () {},
-                    icons: Icons.info_outline_rounded,
+                    icons: Icons.edit_outlined,
                     iconStyle: IconStyle(
                       backgroundColor: Colors.white,
                       iconsColor: Colors.black,
                     ),
-                    title: 'About',
-                    subtitle: "Learn more about the App",
+                    title: "Modify",
+                    subtitle: "Tap to change your data",
+                    onTap: () {},
                     trailing: const Icon(
                       Icons.arrow_forward_ios,
                       color: Colors.grey,
@@ -145,70 +56,156 @@ class _ProfileViewState extends ConsumerState<ProfileView> {
                 ),
               ),
             ),
-            // You can add a settings title
-            MySettingsGroup(
-              settingsGroupTitle: "Account",
-              items: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15.0),
-                    topRight: Radius.circular(15.0),
+          ),
+          MySettingsGroup(
+            items: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+                child: Material(
+                  color: Colors.white,
+                  child: InkWell(
+                    child: SettingsItem(
+                      onTap: () {},
+                      icons: Icons.brush_rounded,
+                      iconStyle: IconStyle(
+                        backgroundColor: Colors.white,
+                        iconsColor: Colors.black,
+                      ),
+                      title: 'Appearance',
+                      subtitle: "Make the App yours",
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                        size: 20,
+                      ),
+                    ),
                   ),
-                  child: Material(
-                    color: Colors.white,
-                    child: InkWell(
-                      child: SettingsItem(
-                        onTap: () {
-                          showQuickAlert(context, onLogOut);
+                ),
+              ),
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                ),
+                child: Material(
+                  color: Colors.white,
+                  child: InkWell(
+                    child: SettingsItem(
+                      onTap: () {
+                        setState(() {
+                          isSwitchOn = !isSwitchOn;
+                        });
+                      },
+                      icons: Icons.dark_mode_outlined,
+                      iconStyle: IconStyle(
+                        backgroundColor: Colors.white,
+                        iconsColor: Colors.black,
+                      ),
+                      title: 'Dark mode',
+                      subtitle: "Automatic",
+                      trailing: Switch.adaptive(
+                        value: isSwitchOn,
+                        onChanged: (newValue) {
+                          setState(() {
+                            isSwitchOn = newValue;
+                          });
                         },
-                        icons: Icons.exit_to_app_rounded,
-                        iconStyle: IconStyle(
-                          iconsColor: Colors.black,
-                          backgroundColor: Colors.white,
-                        ),
-                        title: "Sign Out",
-                        trailing: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.grey,
-                          size: 20,
-                        ),
                       ),
                     ),
                   ),
                 ),
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(15.0),
-                    bottomRight: Radius.circular(15.0),
+              ),
+            ],
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Material(
+              color: Colors.white,
+              child: InkWell(
+                child: SettingsItem(
+                  onTap: () {},
+                  icons: Icons.info_outline_rounded,
+                  iconStyle: IconStyle(
+                    backgroundColor: Colors.white,
+                    iconsColor: Colors.black,
                   ),
-                  child: Material(
-                    color: Colors.white,
-                    child: InkWell(
-                      child: SettingsItem(
-                        onTap: () {},
-                        icons: CupertinoIcons.delete_solid,
-                        iconStyle: IconStyle(
-                          backgroundColor: Colors.white,
-                          iconsColor: Colors.black,
-                        ),
-                        title: "Delete account",
-                        titleStyle: const TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        trailing: const Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.grey,
-                          size: 20,
-                        ),
-                      ),
-                    ),
+                  title: 'About',
+                  subtitle: "Learn more about the App",
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.grey,
+                    size: 20,
                   ),
                 ),
-              ],
+              ),
             ),
-          ],
-        ),
+          ),
+          // You can add a settings title
+          MySettingsGroup(
+            settingsGroupTitle: "Account",
+            items: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+                child: Material(
+                  color: Colors.white,
+                  child: InkWell(
+                    child: SettingsItem(
+                      onTap: () {
+                        showQuickAlert(context, onLogOut);
+                      },
+                      icons: Icons.exit_to_app_rounded,
+                      iconStyle: IconStyle(
+                        iconsColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                      title: "Sign Out",
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                ),
+                child: Material(
+                  color: Colors.white,
+                  child: InkWell(
+                    child: SettingsItem(
+                      onTap: () {},
+                      icons: CupertinoIcons.delete_solid,
+                      iconStyle: IconStyle(
+                        backgroundColor: Colors.white,
+                        iconsColor: Colors.black,
+                      ),
+                      title: "Delete account",
+                      titleStyle: const TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
